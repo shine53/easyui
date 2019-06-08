cd7b688b7ee72780e9a3ea40a95773393d360a5c
shine53 <415094183@qq.com>
2019-06-08 21:24:04 +0800

    2 修该loading 动画；新增页面；window最小化还原效果。

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // $(function () {
-    // loading 
+    // loading
     function closes(dom) {
         dom.fadeOut("normal", function() {
             $(this).remove();
@@ -13,6 +13,7 @@
         }
         loading = setTimeout(closes($("#loading")), 1000);
     }
+
     var menuData = null;
     $.ajax({
         // url: 'http://127.0.0.1:5500/demo/src/data/menu.json',
@@ -20,9 +21,9 @@
         dataType: 'json',
         type: 'get',
         async: false,
-        contentType: 'application/json;charset=utf-8', 
-        success: function(res){  
-            // console.log(res.data); 
+        contentType: 'application/json;charset=utf-8',
+        success: function(res){
+            // console.log(res.data);
             menuData = res.data;
             menuData.forEach(function(item){
                 getMenu(item);
@@ -38,18 +39,18 @@
     // 生成主菜单
     function getMenu(data) {
         var str = '';
-        str += '<a id="menu_'+data.id+'" href="#" class="easyui-linkbutton" iconCls="'+data.iconCls+'" data-options="plain:true">'+data.text+'</a>'
+        str += '<a id="menu_'+data.id+'" href="javascript:;" class="menu-btn easyui-linkbutton" iconCls="'+data.iconCls+'" data-options="plain:true">'+data.text+'</a>'
         $('.easyui-panel').append(str)
         $.parser.parse($('.easyui-panel'))
     }
 
     var curData = [];
-    var $menu_a = $('.easyui-panel').children('a');
-
+    $menu = $('.menu-panel');
+    var $menu_a = $menu.children('a');
     // 点击生成二级菜单
     $menu_a.on('click',function(e) {
         curData = [];
-        var secondeMenu = null, itemData = [], secMenu = null; 
+        var secondeMenu = null, itemData = [], secMenu = null;
 
         var index = $menu_a.index(this);
         secMenu = menuData[index].child[0];
@@ -65,64 +66,48 @@
             children: itemData
         }
         curData.push(secondeMenu);
-        showSecMenu();   
+        showSecMenu();
     })
 
     // 显示二级菜单
     function showSecMenu() {
-        $('.west').empty();
+        $('#west').empty();
         var str = '';
+        // 不加data拿不到#sm？？？？
+        // str += '<div id="sm" class="easyui-sidemenu" style="width:113px;"></div>'
         str += '<div id="sm" class="easyui-sidemenu" style="width:113px;" data-options="data:curData"></div>'
-        // str += '<div id="sm" class="easyui-sidemenu" style="width:113px;" data-options="data:curData"></div>'
-        $('.west').append(str);
-        $.parser.parse($('.west'));
-        var $li = $('.west').find('li');
-        // console.log($li);
-        // 二级菜单绑定事件有点乱，先按习惯写法，等待修改。。。
-        // $('#tt').tabs({
-        //     data: curData,
-        //     onSelect: function() { 
-        //         showContent();
-        //     }
-        // })
-        $li.on('click', showContent);
+        $('#west').append(str);
+        // 报错？？？？
+        $.parser.parse($('#west'));
+
+        var $li = $('#west').find('li');
+        $('#sm').sidemenu({
+            onSelect: function(item) {
+                showContent(item);
+            }
+        })
     }
 
-    function showContent(e) {
+    function showContent(item) {
         // 二级菜单对应的内容
-        console.log(e);
-        // flag 判断是否需要加载动画，等待修改。。。
-        var title = '', content, flag;
-        var url = 'data/content.json';
-        // console.log($(this).find('.tree-title').text());
-        title = $(this).find('.tree-title').text();
-        content = '<iframe scrolling="auto" frameborder="1" src="https://www.baidu.com" style="width:100%;height:1000px;"></iframe>';
-        hasTabs();
-        if(!flag) {
-            var mark = '';
-            mark = '<div class="content-mark"><img src="src/img/loading.gif"/><div>';
-            $('#tt').append(mark);
-            setTimeout(closes($(".content-mark")),5000);
-        }
+        var title = '';
+        title = item.text;
+        hasTabs(title);
 
-        function hasTabs() {
-            // 判断标签是否打开过,且打开过的标签不需要加载动画
+        function hasTabs(title) {
+            // 判断标签是否打开过,打开过的标签不需要加载动画
             if ($('#tt').tabs('exists', title)){
                 $('#tt').tabs('select', title);
-                flag = true;
             } else {
                 $('#tt').tabs('add',{
+                    // 面板的属性
                     title: title,
-                    content: '<div style="padding:10px">'+content+'</div>',
+                    href: './pages/datagrid.html',
+                    closable: true,
+                    // content: '<div style="padding:10px">'+content+'</div>',
                     closable: true
-                }); 
-                flag = false;
+                });
             }
-            $('#tt').tabs('resize', {
-                plain: false,
-                boder: false,
-                height: $(window).height()
-            })
         }
     }
 // })
