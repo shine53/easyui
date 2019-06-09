50eec651858cbe61c7990586373dcb7f36298b10
shine53 <415094183@qq.com>
2019-06-09 23:02:14 +0800

    5 简化了一些代码，修改二级菜单为页面加载的时候显示第一个

--- index.js
+++ index.js
@@ -15,26 +15,33 @@
     }
 
     var menuData = null;
-    $.ajax({
-        // url: 'http://127.0.0.1:5500/demo/src/data/menu.json',
-        url: 'src/data/menu.json',
-        dataType: 'json',
-        type: 'get',
-        async: false,
-        contentType: 'application/json;charset=utf-8',
-        success: function(res){
-            // console.log(res.data);
-            menuData = res.data;
-            menuData.forEach(function(item){
-                getMenu(item);
-            })
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-            console.log(XMLHttpRequest.status);
-            console.log(XMLHttpRequest.readyState);
-            console.log(textStatus);
-        }
-    })
+
+    // $(document).ready(function() {
+		$.ajax({
+	        // url: 'http://127.0.0.1:5500/demo/src/data/menu.json',
+	        url: 'src/data/menu.json',
+	        dataType: 'json',
+	        type: 'get',
+	        async: false,
+	        contentType: 'application/json;charset=utf-8',
+	        success: function(res){
+	            // console.log(res.data);
+	            menuData = res.data;
+	            menuData.forEach(function(item){
+	                getMenu(item);
+	            })
+	        },
+	        error: function (XMLHttpRequest, textStatus, errorThrown) {
+	            console.log(XMLHttpRequest.status);
+	            console.log(XMLHttpRequest.readyState);
+	            console.log(textStatus);
+	        }
+	    })
+	// })
+
+    var curData = null;
+    $menu = $('.menu-panel');
+    var $menu_a = $menu.children('a');
 
     // 生成主菜单
     function getMenu(data) {
@@ -42,17 +49,14 @@
         str += '<a id="menu_'+data.id+'" href="javascript:;" class="menu-btn easyui-linkbutton" iconCls="'+data.iconCls+'" data-options="plain:true">'+data.text+'</a>'
         $('.easyui-panel').append(str)
         $.parser.parse($('.easyui-panel'))
+		createSecMenu();
     }
-
-    var curData = [];
-    $menu = $('.menu-panel');
-    var $menu_a = $menu.children('a');
-    // 点击生成二级菜单
-    $menu_a.on('click',function(e) {
-        curData = [];
+	function createSecMenu(index) {
+		index = index || 0;
+		
+		curData = [];
         var secondeMenu = null, itemData = [], secMenu = null;
 
-        var index = $menu_a.index(this);
         secMenu = menuData[index].child[0];
         // console.log(secMenu);
         secMenu.child.forEach(function(item){
@@ -62,31 +66,29 @@
         secondeMenu = {
             text: secMenu.text,
             iconCls: 'icon-sum',
-            // state: 'open',
+            state: 'open',
             children: itemData
         }
         curData.push(secondeMenu);
-        showSecMenu();
+		showSecMenu();
+	}
+    // 点击生成二级菜单
+    $menu_a.on('click',function(e) {
+		// console.log(1);
+        var index = $menu_a.index(this);
+		createSecMenu(index);
     })
 
     // 显示二级菜单
     function showSecMenu() {
-        $('#west').empty();
-        var str = '';
-        // 不加data拿不到#sm？？？？
-        // str += '<div id="sm" class="easyui-sidemenu" style="width:113px;"></div>'
-        str += '<div id="sm" class="easyui-sidemenu" style="width:113px;" data-options="data:curData"></div>'
-        $('#west').append(str);
-        // 报错？？？？
-        $.parser.parse($('#west'));
-
-        var $li = $('#west').find('li');
         $('#sm').sidemenu({
+			data: curData,
             onSelect: function(item) {
                 showContent(item);
             }
-        })
-    }
+        });
+		$.parser.parse($('#sm'));
+    };
 
     function showContent(item) {
         // 二级菜单对应的内容
